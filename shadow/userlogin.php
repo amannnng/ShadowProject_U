96427690d3d97646b51529e13049fd9b8d8e07e0
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-07 17:36:27 -0500

    d
    w

--- shadow/userlogin.php
+++ shadow/userlogin.php
@@ -200,7 +200,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				  </div>
 				  <div class="form">
 					<h2>Login to your account</h2>
-					<form action="userlogin.php" method="post">
+					<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
 					  <input type="text" name="username" placeholder="Username" required=" ">
 					  <input type="password" name="password" placeholder="Password" required=" ">
 					  <input type="submit" value="Login">

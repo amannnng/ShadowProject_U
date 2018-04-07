b508dd5754e641e2e63c8fcf01e2166a4f6343a0
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-07 17:31:12 -0500

    login
    login

--- shadow/userlogin.php
+++ shadow/userlogin.php
@@ -200,7 +200,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				  </div>
 				  <div class="form">
 					<h2>Login to your account</h2>
-					<form action="login.php" method="post">
+					<form action="userlogin.php" method="post">
 					  <input type="text" name="username" placeholder="Username" required=" ">
 					  <input type="password" name="password" placeholder="Password" required=" ">
 					  <input type="submit" value="Login">
@@ -232,8 +232,8 @@ if(!empty($_POST['username']) && !empty($_POST['password']))
 		{
 			session_start();
 			$_SESSION['name']=$username;
-			print_r($_SESSION);	
-			//header('Location: index.php');
+			//print_r($_SESSION);	
+			header('Location: index.php');
 		}
 		
 	}

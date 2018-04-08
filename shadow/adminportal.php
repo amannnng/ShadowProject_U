c208b7806debad1763fbc7502be5ec9fa96a88a8
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-08 17:58:01 -0500

    f

--- shadow/adminportal.php
+++ shadow/adminportal.php
@@ -87,6 +87,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				   <div class="form">
 					<h2>Add Products</h2>
 					<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+					<form action="upload.php" method="post" enctype="multipart/form-data">
+						Select image to upload:
+						<input type="file" name="fileToUpload" id="fileToUpload">
+					<!--	<input type="submit" value="Upload Image" name="submit">-->
+					</form>
 					<input type="text" name="brand" placeholder="Brand Name" required=" ">
 					<input type="text" name="product" placeholder="Product Name" required=" ">
 						<select name="category">
@@ -104,11 +109,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<input type="text" name="pricebefore" placeholder="Price Before" required=" ">
 					<input type="text" name="priceafter" placeholder="Price After" required=" ">
 					<input type="date" name="offerexp" placeholder="offerexp" required=" "><br>
-					<form action="upload.php" method="post" enctype="multipart/form-data">
-						Select image to upload:
-						<input type="file" name="fileToUpload" id="fileToUpload">
-					<!--	<input type="submit" value="Upload Image" name="submit">-->
-					</form>
+					
 					<!--<input type="text" name="imgurl" placeholder="Price After" required=" ">-->
 					<br>
 						<select name="storelocation">

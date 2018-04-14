bd87c75717801dbf99db6b14eea527112b24cd4c
amannnng <amannnng@gmail.com>
2018-04-13 21:30:51 -0500

    update

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			</div>
 			<div class="w3ls_logo_products_left1">
 				<ul class="special_items">
-					<p>Get Great Deals on Electronics, Home Appliances </p>
+					<p>Get Great Deals on many products </p>
 				</ul>
 			</div>
 			<div class="w3ls_logo_products_left1">

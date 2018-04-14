443d359eefd285a37f906330d2ed8151aa482cf7
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-13 19:07:23 -0500

    njdf

--- index.php
+++ index.php
@@ -242,7 +242,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 										<div class="snipcart-thumb">';
 										$id = $res["_id"];
 										echo $id;
-										echo'<a href="single.php?id="><img title=" " alt=" " src="'.$res["imgurl"].'" height="50px" width="50px"/></a>		
+										echo'<a href="single.php?id='.$id.'"><img title=" " alt=" " src="'.$res["imgurl"].'" height="50px" width="50px"/></a>		
 											<p>'.$res["brand"].'</p><p>'.$res["product"].'</p><p>'.$res["description"].'</p><p>'.$res["storelocation"].'</p>
 											<h4>$'.$res["priceafter"].'<span>$'.$res["pricebefore"].'</span></h4>
 										</div>

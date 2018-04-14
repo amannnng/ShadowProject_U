93673f0707570cf205f32aae9a5b60f92c50de8d
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-13 19:04:31 -0500

    g

--- index.php
+++ index.php
@@ -241,6 +241,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 									<div class="snipcart-item block" >
 										<div class="snipcart-thumb">';
 										$id = $res["_id"];
+										echo $id;
 										echo'<a href="single.php?id="><img title=" " alt=" " src="'.$res["imgurl"].'" height="50px" width="50px"/></a>		
 											<p>'.$res["brand"].'</p><p>'.$res["product"].'</p><p>'.$res["description"].'</p><p>'.$res["storelocation"].'</p>
 											<h4>$'.$res["priceafter"].'<span>$'.$res["pricebefore"].'</span></h4>

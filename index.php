5385aeebe0a104b4f0c9ae4d9f3adadf1908c738
amannnng <amannnng@gmail.com>
2018-04-13 22:25:27 -0500

    Update the banner display

--- index.php
+++ index.php
@@ -166,26 +166,26 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					<ul class="slides">
 						<li>
 							<div class="w3l_banner_nav_right_banner">
-								<h3>Make your <span>food</span> with Spicy.</h3>
+								<!--<h3>Make your <span>food</span> with Spicy.</h3>
 								<div class="more">
 									<a href="products.html" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
-								</div>
+								</div>-->
 							</div>
 						</li>
 						<li>
 							<div class="w3l_banner_nav_right_banner1">
-								<h3>Make your <span>food</span> with Spicy.</h3>
+								<!--<h3>Make your <span>food</span> with Spicy.</h3>
 								<div class="more">
 									<a href="products.html" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
-								</div>
+								</div>-->
 							</div>
 						</li>
 						<li>
 							<div class="w3l_banner_nav_right_banner2">
-								<h3>upto <i>50%</i> off.</h3>
+								<!--<h3>upto <i>50%</i> off.</h3>
 								<div class="more">
 									<a href="products.html" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
-								</div>
+								</div>-->
 							</div>
 						</li>
 					</ul>

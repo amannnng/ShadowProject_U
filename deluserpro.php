df877e5565b9be18faa80b49d0631061b4367b0e
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-15 18:06:13 -0500

    ew

--- deluserpro.php
+++ deluserpro.php
@@ -2,17 +2,12 @@
 <?php
 // connect to mongodb
    $m = new MongoClient();
-   // select a database
    $db = $m->dealhunter;
    $collection = $db->userinfo;
 	$id=$_GET["id"];
-	session_start();
-	$username=$_SESSION["name"];
-	$row=$collection->find(array("productid"=>new MongoId($id)));
-	
-	
-	$collection->remove(array("_id"=>new MongoId($id)));
-		
+//	$row=$collection->find(array("productid"=>$id));
+	$collection->remove("productid"=>$id);
+		echo''
 //	echo'<script language="javascript" type="text/javascript">alert("Your Account Has Been Deleted Successfully.");</script>';
 //	header("location:useraccount.php");
 ?>

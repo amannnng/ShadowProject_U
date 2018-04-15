24631ceeb4385747c8c93742b0c7fc50598f73f1
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-15 18:07:18 -0500

    ew

--- deluserpro.php
+++ deluserpro.php
@@ -6,7 +6,7 @@
    $collection = $db->userinfo;
 	$id=$_GET["id"];
 //	$row=$collection->find(array("productid"=>$id));
-	$collection->remove("productid"=>$id);
+	$collection->remove(array("productid"=>$id_);
 		echo''
 //	echo'<script language="javascript" type="text/javascript">alert("Your Account Has Been Deleted Successfully.");</script>';
 //	header("location:useraccount.php");

d03ca7b056bfd5e7e91fcc742fb28347e8637ad8
amannnng <amannnng@gmail.com>
2018-04-14 22:14:59 -0500

    re

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			</div>
 			<div class="w3ls_logo_products_left1">
 				<ul class="special_items">
-					<p style="color:DodgerBlue;">Get Great Deals on many offers... </p>
+					<p style="color:DodgerBlue;">Get Great Deals on many products </p>
 				</ul>
 			</div>
 			<div class="w3ls_logo_products_left1">

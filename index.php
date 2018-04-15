6926c61a3c7e889db8f370d89c0f7ca4960e4584
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-15 12:55:01 -0500

    g

--- index.php
+++ index.php
@@ -83,12 +83,13 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     <input type="hidden" name="display" value="1" />
                     <input type="submit" name="submit" value="View your cart" class="button" />
                 </fieldset>
-            </form><a color="white">Hi, <?Php echo $uname?></a>
+            </form>
 		</div>
 		<div class="w3l_header_right">
 			<ul>
 				<li class="dropdown profile_details_drop">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i><span class="caret"></span></a>
+					<a color="white">Hi, <?Php echo $uname?></a>
 					<div class="mega-dropdown-menu">
 						<div class="w3ls_vegetables">
 							<ul class="dropdown-menu drp-mnu">

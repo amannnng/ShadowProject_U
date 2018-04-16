3f76212825676b9ef7cd2397d143f48c82dc699b
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-15 20:39:28 -0500

    rt

--- index.php
+++ index.php
@@ -288,7 +288,7 @@ else{
 										</div>
 										<div class="snipcart-details top_brand_home_details">
 											<form action="checkout.php?id='.$id.'" method="post">';
-											 if(isset($_GET["msg1"])){echo $_GET["msg1"]; msg=null;}
+											 if(isset($msg=$_GET["msg1"])){echo $msg; $msg=null;}
 											echo'	<input type="submit" name="submit" value="Save for Later" class="button" />													
 											</form>
 									

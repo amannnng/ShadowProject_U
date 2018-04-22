af50ad11b2db34dcce71395e607e4dad3b677e7a
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-22 17:20:21 -0500

    re

--- search.php
+++ search.php
@@ -183,7 +183,7 @@ echo'<div align="center">';
 	//$product = new MongoRegex("/$_POST['product']/i");
     //$product = new \MongoDB\BSON\Regex(preg_quote($_POST['product']), 'i');
 	//$row = $collection->find(array('$or' => array(array("brand" =>$product),array("product" =>$product))));
-	  $row = $collection->find(array('$or' => array(array("brand" =>$product),array("product" =>$product))));
+	  $row = $collection->find(array('$or' => array(array("brand" =>new MongoRegex("/$product/i")),array("product" =>new MongoRegex("/$product/i")))));
 	//$row=$collection->find(array("brand"=>$product,"product"=>$product));
 //echo $date;
 		

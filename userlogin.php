286f960da8c1b20a8173475c8b5bc53444788358
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-22 15:32:14 -0500

    re

--- userlogin.php
+++ userlogin.php
@@ -123,11 +123,11 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	</div>
 <!-- //products-breadcrumb -->
 <!-- banner -->
-	<div class="banner">
+	<!--<div class="banner">
 		<div class="w3l_banner_nav_left">
 		<nav class="navbar nav_bottom">
 			 <!-- Brand and toggle get grouped for better mobile display -->
-			  <div class="navbar-header nav_2">
+			    <!--<div class="navbar-header nav_2">
 				  <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
@@ -135,7 +135,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					<span class="icon-bar"></span>
 				  </button>
 			   </div> 
-			   <!-- Collect the nav links, forms, and other content for toggling -->
+			  Collect the nav links, forms, and other content for toggling -->
 				<!--<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
 					<ul class="nav navbar-nav nav_1">
 					
@@ -150,9 +150,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					
 					</ul>
 				 </div>--><!-- /.navbar-collapse -->
-			</nav>
+		<!--	</nav>
 		</div>
-		<div class="w3l_banner_nav_right">
+		<div class="w3l_banner_nav_right"> -->
 	
 <!-- login -->
 

00d4f1205b02bb5dbe11c3b6bda314f82d366561
Aishwarya Desai <aishwaryaudesai17@gmail.com>
2018-04-22 15:21:57 -0500

    ere

--- adminportal.php
+++ adminportal.php
@@ -9,9 +9,9 @@ $x=false;
 foreach($row as $res)
 	{
 		if($res['username']==$check)
-		$x=true;
+		$x=false;
 	}
-if(!isset($_SESSION["name"]) && $x!=true)
+if(!isset($_SESSION["name"]) && $x==false)
 {
 	header('Location: adminlogin.php');
 }

3087309ad6a67bfbea9fa4c002975d0d314b43dc
amannnng <amannnng@gmail.com>
2018-04-23 15:35:45 -0500

    re

--- single.php
+++ single.php
@@ -320,7 +320,7 @@ $id=$_GET["id"];
 				<div class="col-md-8 agileinfo_single_right">
 					<div class="w3agile_description">
 						<h4>Description :</h4>
-						<textarea>'.$res["description"].'.</textarea>
+						<textarea cols="50">'.$res["description"].'.</textarea>
 					</div>
 					<div class="snipcart-item block">
 						<div class="snipcart-thumb agileinfo_single_right_snipcart">

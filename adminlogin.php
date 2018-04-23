737d8937c8cbd3fd7ee407a2ff05b180703603d0
amannnng <amannnng@gmail.com>
2018-04-23 02:28:33 -0500

    re

--- adminlogin.php
+++ adminlogin.php
@@ -133,7 +133,7 @@ if(!empty($_POST['username']) && !empty($_POST['password']))
 			<div class="w3_login_module">
 				<div class="module form-module">
 				  <div class="toggle"><i class="fa fa-times fa-pencil"></i>
-					<div class="tooltip">Click Me</div>
+					
 				  </div>
 				  <div class="form">
 					<h2>Login to your account</h2>
